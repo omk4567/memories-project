ed07b8a9d7137e3a007c7815502220b75be76148
Omkar <omkaraher2002@gmail.com>
2024-09-10 14:38:50 +0530

    server changes made

--- server/index.js
+++ server/index.js
@@ -7,12 +7,21 @@ import postRoutes from './routes/posts.js'
 
 const app = express();      
 dotenv.config();  
+const allowedOrigins = ['https://my-memories-project123.netlify.app', 'http://localhost:3000']; 
 
 app.use(bodyParser.json({limit: "50mb", extended: "true"}));      
 app.use(bodyParser.urlencoded({limit: "50mb", extended: "true"}));   
 app.use(cors({
-    origin: 'http://localhost:3000', // Your frontend URL
-    credentials: true
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+    
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));   
 
 app.use('/posts', postRoutes);
